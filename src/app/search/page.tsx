17482738142594d80c40b32f7276741f1f0459e1
mianglouch123 <miangpermer@gmail.com>
2024-03-15 12:08:40 -0500

    fix suspense in search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -6,24 +6,31 @@ import {  useSearchParams } from "next/navigation";
 import Results from "../../Components/Results";
 import { Suspense } from "react";
 
-function Page() {
+
+function Search() {
 
 const params = useSearchParams()
 const query = params.get('q') as string
+      
+      
+      return (
+<div className="h-100vh w-100vw flex justify-center items-center flex-col p-4 pl-[15px]">
 
+<Results query={query}/>
 
+</div>
+       )
+}
+
+function PageSearch() {
 
 return (
         
        <Suspense>
-        <div className="h-100vh w-100vw flex justify-center items-center flex-col p-4 pl-[15px]">
-
-        <Results query={query}/>
         
-        </div>
-
+       <Search />
        </Suspense>
 )
 }
 
-export default Page
+export default PageSearch
